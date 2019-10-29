7f2027779cd0e02a030a29e89b8465c132daffbe
Guru Raj <gururaj.aras@gmail.com>
2019-10-29 21:05:51 +0530

    updated database

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 mongoose.Promise = global.Promise
-mongoose.connect('mongodb://gururaj.aras@gmail.com:Guru1421@cluster0-shard-00-00-a90ax.mongodb.net:27017,cluster0-shard-00-01-a90ax.mongodb.net:27017,cluster0-shard-00-02-a90ax.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority')
+mongoose.connect('mongodb://gururajhshetty5:Guru1421@cluster0-shard-00-00-a90ax.mongodb.net:27017,cluster0-shard-00-01-a90ax.mongodb.net:27017,cluster0-shard-00-02-a90ax.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority')
     .then(() => {
         console.log('successfully connected to database')
     })
