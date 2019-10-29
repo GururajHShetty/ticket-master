20ba7cee85de8a49d3a97b2786039d889f2874a9
Guru Raj <gururaj.aras@gmail.com>
2019-10-29 19:29:17 +0530

    deploying to heroku

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express')
-const path = require('path') 
 const app=express()
-const port = process.env.PORT || 3020
+const port = 3020
 const mongoose = require('./config/database')
 const router = require('./config/routes')
 
@@ -10,18 +9,14 @@ app.use(cors())
 
 app.use(express.json())
 
-app.get('/',(req,res)=>{
-    res.json({
-        notice:'Welcome to ticket master'
-    })
-})
+// app.get('/',(req,res)=>{
+//     res.json({
+//         notice:'Welcome to ticket master'
+//     })
+// })
 
 app.use('/',router)
 
-app.use(express.static(path.join(__dirname,"client/build"))) 
-app.get("*",(req,res) => { 
-    res.sendFile(path.join(__dirname + "/client/build/index.html")) 
-})
 
 app.listen(port,() => {
     console.log('listening on port',port)
